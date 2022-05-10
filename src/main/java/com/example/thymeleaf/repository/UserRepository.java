e1e7a3291888fd126b353347fd27022d407c51e4
MarFor9 <marcin_formela@o2.pl>
2022-05-10 09:37:41 +0200

    some changes

--- src/main/java/com/example/thymeleaf/repository/UserRepository.java
+++ src/main/java/com/example/thymeleaf/repository/UserRepository.java
@@ -2,9 +2,7 @@ package com.example.thymeleaf.repository;
 
 import com.example.thymeleaf.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository("userRepository")
 public interface UserRepository extends JpaRepository<User, String> {
     User findUserByEmailIdIgnoreCase(String emailId);
 }
